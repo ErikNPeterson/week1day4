9281d297e546153bb2df16916269a38cb615ba45
vagrant <erikeh1@gmail.com>
2018-10-25 18:55:34 +0000

    improved callbacksMap under 2callbacksMap.js

--- callbacksMap.js
+++ callbacksMap.js
@@ -1,10 +1,5 @@
 var words = ["ground", "control", "to", "major", "tom"];
 
-
-
-
-// these are calling the map function above // these are not the (map)method.
-
 map(words, function(word) { // words is our array at the top
   return word.length; // function(word) is sent to callback 
 });
@@ -19,9 +14,9 @@ map(words, function(word) {
 
 
 function map(array, callback){
-  var newArray =[];
-  array.forEach(element => {
-  newArray.push(callback(element)); // think about using map to make the new array
-  });
- console.log(newArray);
+  console.log(array.map(element => callback(element)))
 }
+
+
+
+
