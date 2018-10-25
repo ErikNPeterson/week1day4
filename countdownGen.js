9edc7cb4630a1d78a4b587002315b218d8e4935c
vagrant <erikeh1@gmail.com>
2018-10-25 20:06:51 +0000

    countdown generator

---
+++ countdownGen.js
@@ -0,0 +1,42 @@
+var countdownGenerator = function (x) {
+  
+  var counter = x;
+  
+  return function (x){
+    if (counter > 0){
+      console.log("T-minus " + counter + "...")
+    } else if (counter === 0){
+      console.log("Blast Off!")
+    } else {
+      console.log("Rockets already gone, bub!")
+    }
+    counter -= 1;
+  }
+
+};
+
+var countdown = countdownGenerator(15);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
